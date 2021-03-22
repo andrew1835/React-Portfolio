2a37b241fcb4e09285780ae38718e51f761c5405
Andrew Anthony <a.anthony1835@gmail.com>
2021-03-21 21:59:36 -0700

    expanded description

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -25,35 +25,26 @@ function About() {
                     <section className="col-md-8">
 
                         <p>Hi, my name is Andrew Anthony and I'm a front-end web developer with a certificate in full-stack developement from the University of Washington's coding bootcamp.
-            <br></br>
-                                    I graduated from the University of Washington in June of 2020 with a degree in Marketing, however,
-                                    my plan was to pursue a career in screenwriting and acting post graduation. In fact, during the fall
-                                    of 2019, I was a Script Intern for Conan O' Brien in Los Angeles. Upon completing the internship, I
+            <br></br><br></br>
+                                    I earned my bachelor's degree in marketing from the University of Washington in June of 2020 with interdiscplinary honors, however,
+                                    my original plan was to pursue a career in screenwriting and acting post graduation. In fact, during my senior year I took a quarter off to be a Script Intern for Conan O' Brien in Los Angeles. Upon completing the internship, I
                                     decided that after I graduated from college, I would move back down to LA to pursue acting and
                                     screenwriting. Then COVID hit, and as with so many other people, I had to adjust and come up with a
                                     new plan.
-                                    I spent a lot of time reflecting, and realized that, although I had enjoyed the small amount of
-                                    coding I had been exposed to in my life, I had never seriously considered a career incoding. I had
-                                    written it off years ago because I thought that it wasn't a creative path, but after completigin
+                                    I spent a lot of time reflecting, and I realized that I had never seriously considered a career in coding. I had
+                                    written it off years ago because I thought that it wasn't a creative path, but after completing
                                     some coding tutorials, I discovered how innately creative coding truly is. I decided to go all in
-                                    and signed up for this bootcamp, and I'm confident that upon graduation I'll be Employer-Competitive
-                                    and looking ready to make an impact as a web developer.
+                                    and signed up for UW's coding bootcamp. I couldn't be happier with the results. It was challenging and more demanding than any job or class I've ever had before, but it was well worth it. Week after week I fell more in love with the creativity, problem-solving, and continual growth that is intrinsic to coding as a discpline, and I'm extremely excited to continue down this path as a software engineer.
         </p>
                     </section>
                 </section>
 
-                <br></br>
+
 
 
                 <section className="row">
                     <section className="col-md-12">
-                        <p>
-                            I don't know where the future will take me exactly, but that's the fun of life. All I can do is make
-                            sure I've acquired the skills necessary to navigate life and rise to meet opportunities that cross
-                            my path. I'm confident that, when it comes to jobs, having my combination of storytelling,
-                            marketing, and programming skills will make me a strong employee candidate, and a valuable addition
-                            to any company that will hire me.
-        </p>
+
                         <br></br>
                         <p>Resume: <a href={Image} target="_blank">Click here to download</a>
                         </p>
